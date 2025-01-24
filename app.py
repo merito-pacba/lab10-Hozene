3d456c1b428d33093cdc18cbd586d4caf4230ce3
Jędrzej <39808512+Hozene@users.noreply.github.com>
2025-01-24 04:47:55 +0000

    Add skyscraper ad blocks

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import pyodbc
 from flask import (Flask, render_template, request)
+import random
 
 load_dotenv()
 
@@ -19,13 +20,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+
+   ads = ['ad1.mp4', 'ad2.mp4', 'ad3.mp4', 'ad4.mp4']
+   selected_ad = random.choice(ads)
+
+   query = """ 
+      SELECT SUM(amount) AS total_spent
+      FROM Transactions
+      WHERE user_id = 1 AND date >= DATEADD(day, -30, GETDATE())
+   """
+   cursor.execute(query)
+   total = cursor.fetchone()
+   total_spent = total[0] if total[0] else 0
+   return render_template('index.html', total=total_spent, selected_ad=selected_ad)
 
 @app.route('/transactions')
 def transactions():
+
+   ads = ['ad1.mp4', 'ad2.mp4', 'ad3.mp4', 'ad4.mp4']
+   selected_ad = random.choice(ads)
+
    try:
       query = """
-         SELECT TOP 10 t.amount, t.description, t.date, c.name AS category_name
+         SELECT TOP 25 t.amount, t.description, t.date, c.name AS category_name
          FROM Transactions AS t
          JOIN Categories AS c ON t.category_id = c.id
          WHERE t.user_id = 1
@@ -33,7 +50,7 @@ def transactions():
       """
       cursor.execute(query)
       transactions = cursor.fetchall()
-      return render_template('transactions.html', transactions=transactions)
+      return render_template('transactions.html', transactions=transactions, selected_ad=selected_ad)
    
    except Exception as e:
       print(f"Error: {e}")
